5b9039dfa5ec2bc88dedaea139bf58a837f4425a
Rami Alrwais <raalrwai@ucsc.edu>
2026-02-03 09:04:52 -0500

    Agent Interface

---
+++ Day15/orchestrator.py
@@ -0,0 +1,9 @@
+from typing import Dict, List
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from rouge_score import rouge_scorer
+
+class Agent():
+    def run(self, state: Dict)-> Dict:
+        pass
+
+
